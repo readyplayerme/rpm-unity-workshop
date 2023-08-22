3f304aa4798e799cd3cf747c5732d793969aa901
Sercan Altundas <srcnalt@users.noreply.github.com>
2023-08-22 18:55:11 +0300

    feat: security room setup

---
+++ Assets/Scenes/AvatarLoader.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using ReadyPlayerMe.Core;
+
+public class AvatarLoader : MonoBehaviour
+{
+    [SerializeField] private string avatarUrl;
+    [SerializeField] PlayerMovement playerMovement;
+    [SerializeField] private RuntimeAnimatorController animatorController;
+    [SerializeField] private GameObject disguise;
+    
+    private void Start()
+    {
+        AvatarObjectLoader loader = new AvatarObjectLoader();
+        loader.LoadAvatar(avatarUrl);
+        loader.OnCompleted += OnAvatarLoaded;
+    }
+
+    private void OnAvatarLoaded(object sender, CompletionEventArgs e)
+    {
+        GameObject avatar = e.Avatar;
+        
+        avatar.transform.SetParent(transform);
+        avatar.transform.localPosition = Vector3.zero;
+        avatar.transform.localRotation = Quaternion.identity;
+        
+        Animator animator = e.Avatar.GetComponent<Animator>();
+        animator.runtimeAnimatorController = animatorController;
+        
+        playerMovement.animator = animator;
+        playerMovement.enabled = true;
+
+        Transform headTop = GameObject.Find("HeadTop_End").transform;
+        disguise.transform.SetParent(headTop);
+    }
+}
