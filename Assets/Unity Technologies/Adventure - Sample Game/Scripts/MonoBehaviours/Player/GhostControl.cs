41bf4c90993bed597cdcb96e90a8414bb06e173e
Sercan Altundas <srcnalt@users.noreply.github.com>
2023-08-24 15:12:49 +0300

    feat: cleaup

--- Assets/Unity Technologies/Adventure - Sample Game/Scripts/MonoBehaviours/Player/GhostControl.cs
+++ Assets/Unity Technologies/Adventure - Sample Game/Scripts/MonoBehaviours/Player/GhostControl.cs
@@ -1,12 +1,29 @@
+using System.Linq;
 using UnityEngine;
 
 public class GhostControl : MonoBehaviour
 {
+    private void Awake()
+    {
+        Component[] components = transform.parent.GetComponents<Component>();
+
+        if(components.All(c => c.name != "AvatarLoader"))
+        {
+            StartScene();
+        }
+    }
+
     private void OnDisable()
     {
-        // Start scene
+        StartScene();
+    }
+    
+    private void StartScene()
+    {
         SceneController sceneController = FindObjectOfType<SceneController>();
-        MonoBehaviour mb = transform.parent.GetComponent<MonoBehaviour>();
-        mb.StartCoroutine(sceneController.Fade(0));
+        if (sceneController != null)
+        {
+            sceneController.StartCoroutine(sceneController.Fade(0));
+        }
     }
 }
